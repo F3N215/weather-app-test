d6828809e2ddc798da91c980a83e838650333876
Daniel John <dcampo85@gmail.com>
2023-12-27 18:25:27 -0500

    completing weather search

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,31 +10,9 @@
 
 //     if(response.status == 404){
 //         document.querySelector(".error").style.display = "block";
-//         document.querySelector(".error").style.display = "none";
+//         document.querySelector(".weather").style.display = "none";
 //     }else{
-
-//     if(data.weather[0].main == "Clouds"){
-//         weatherIcon.src = "images/clouds.png";
-//     }
-//     else if (data.weather[0].main == "Clear"){
-//         weatherIcon.src = "images/clear.png";
-//     }
-//     else if (data.weather[0].main == "Rain"){
-//         weatherIcon.src = "images/rain.png";
-//     }
-//     else if (data.weather[0].main == "Drizzle"){
-//         weatherIcon.src = "images/drizzle.png";
-//     }
-//     else if (data.weather[0].main == "Mist"){
-//         weatherIcon.src = "images/mist.png";
-//     }
-//     else if (data.weather[0].main == "Snow"){
-//         weatherIcon.src = "images/snow.png";
-//     }
-
-//     document.querySelector(".weather").style.display = "block";
-//     }
-//     var data = await response.json();
+//         var data = await response.json();
     
 //     document.querySelector(".city").innerHTML = data.name;
 //     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
@@ -62,7 +40,7 @@
 
 //     document.querySelector(".weather").style.display = "block";
 //     document.querySelector(".error").style.display = "none";
-
+//     }
 // }
 
 // searchBtn.addEventListener("click", ()=>{
