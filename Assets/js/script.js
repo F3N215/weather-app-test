71e16009d03db1d34f4d2a571ad2a7c9e358eff6
Daniel John <dcampo85@gmail.com>
2023-12-27 17:17:11 -0500

    inline js 2

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,10 +2,15 @@
 // const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=paris";
 
 // async function checkWeather(){
-//     const response = await fetch(apiUrl) + `&appid=${apiKey}`;
+//     const response = await fetch(apiUrl + `&appid=${apiKey}`);
 //     var data = await response.json();
 
 //     console.log(data);
+//     document.querySelector(".city").innerHTML = data.name;
+//     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+//     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+//     document.querySelector(".wind").innerHTML = data.wind.speed + " km/hr";
+
 // }
 
 // checkWeather();
